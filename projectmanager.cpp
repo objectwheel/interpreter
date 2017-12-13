e5aa19368f980ed267af1546310370f32e73a935
Ömer Göktaş <omer@omergoktas.com>
2017-12-13 03:25:50 +0300

    hotfix

--- projectmanager.cpp
+++ projectmanager.cpp
@@ -5,17 +5,15 @@
 #include <QCoreApplication>
 #include <QByteArray>
 
-#define DEFAULT_PROJECT_DIR ("project")
-
 QString ProjectManager::projectDirectory()
 {
     QString baseDir;
     #if defined(Q_OS_ANDROID)
-      baseDir = QString("assets:");
+      baseDir = QString("assets:/");
     #elif defined(Q_OS_IOS) || defined(Q_OS_WINPHONE)
       baseDir = QStandardPaths::standardLocations(QStandardPaths::DataLocation).value(0);
     #else
       baseDir = QCoreApplication::applicationDirPath();
     #endif
-    return baseDir + separator() + DEFAULT_PROJECT_DIR;
+    return baseDir;
 }
