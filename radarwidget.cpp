1071fed46e9cb417f9fee100fbac88bc537f0ee0
Ömer Göktaş <omer@omergoktas.com>
2019-01-26 23:38:46 +0300

    DiscoveryManager - 7
    Signed-off-by: Ömer Göktaş <omer@omergoktas.com>

--- radarwidget.cpp
+++ radarwidget.cpp
@@ -5,74 +5,125 @@
 
 RadarWidget::RadarWidget(QWidget* parent) : QWidget(parent)
 {
-    m_circleRadiusAnimation.setStartValue(0.3);
-    m_circleRadiusAnimation.setEndValue(1.0);
-    m_circleRadiusAnimation.setLoopCount(-1);
-    m_circleRadiusAnimation.setDuration(3000);
-    m_circleRadiusAnimation.setEasingCurve(QEasingCurve::OutQuart);
-    m_circleRadiusAnimation.start();
-//    connect(&m_circleRadiusAnimation, &QVariantAnimation::valueChanged,
-//            this, QOverload<>::of(&RadarWidget::update));
+    m_waveAnimation.setLoopCount(-1);
+    m_waveAnimation.setStartValue(0.0);
+    m_waveAnimation.setEndValue(1.0);
+    m_waveAnimation.setEasingCurve(QEasingCurve::OutQuart);
 
-    m_needleAnimation.setStartValue( - M_PI / 2.0);
-    m_needleAnimation.setEndValue(2 * M_PI + M_PI / 2.0 + M_PI);
     m_needleAnimation.setLoopCount(-1);
-    m_needleAnimation.setDuration(4000);
+    m_needleAnimation.setStartValue(-0.5 * M_PI);
+    m_needleAnimation.setEndValue(3.5 * M_PI);
     m_needleAnimation.setEasingCurve(QEasingCurve::Linear);
-    m_needleAnimation.start();
-    connect(&m_needleAnimation, &QVariantAnimation::valueChanged,
-            this, QOverload<>::of(&RadarWidget::update));
+
+    connect(&m_waveAnimation, &QVariantAnimation::stateChanged, this, &RadarWidget::stateChanged);
+
+    start();
 }
 
-QSize RadarWidget::sizeHint() const
+QAbstractAnimation::State RadarWidget::state() const
 {
-    return {320, 320};
+    return m_waveAnimation.state();
+}
+
+int RadarWidget::period() const
+{
+    return m_waveAnimation.duration() / 2;
+}
+
+void RadarWidget::start(int period)
+{
+    m_needleAnimation.setDuration(2 * period);
+    m_waveAnimation.setDuration(2 * period);
+    m_needleAnimation.start();
+    m_waveAnimation.start();
+    m_updateTimer.start(REFRESH_RATE, this);
+}
+
+void RadarWidget::stop()
+{
+    m_updateTimer.stop();
+    m_waveAnimation.stop();
+    m_needleAnimation.stop();
+}
+
+void RadarWidget::pause()
+{
+    m_updateTimer.stop();
+    m_waveAnimation.pause();
+    m_needleAnimation.pause();
+}
+
+void RadarWidget::resume()
+{
+    m_needleAnimation.resume();
+    m_waveAnimation.resume();
+    m_updateTimer.start(REFRESH_RATE, this);
+}
+
+void RadarWidget::timerEvent(QTimerEvent* event)
+{
+    if (event->timerId() == m_updateTimer.timerId())
+        update();
+    else
+        QWidget::timerEvent(event);
 }
 
 void RadarWidget::paintEvent(QPaintEvent*)
 {
-    QPointF center(QRectF(rect()).center());
-    const qreal circleRadiusFactor = m_circleRadiusAnimation.currentValue().toReal();
+    QColor waveColor = "#f0f0f0";
+    QColor baseColor = "#4ba086";
+    QColor frameColor = "#ffffff";
+    QColor frameBorderColor = "#c7c7c7"; // #242a2d for light mode
+    QColor circleColor = "#50ffffff";
+    QColor needleColor = "#ffffff";
+    QColor scanningColor = "#77ffd4";
+
+    const QPointF center(QRectF(rect()).center());
+    const qreal radius = qMin(width(), height()) / 2.0;
+    const qreal waveFactor = m_waveAnimation.currentValue().toReal();
     const qreal needleFactor = m_needleAnimation.currentValue().toReal();
-    const qreal length = qMin(width(), height());
-    const qreal circleRadius = circleRadiusFactor * length / 2.0 - 0.75;
-    QColor circleBorderColor(palette().windowText().color());
-    circleBorderColor.setAlphaF(1 - circleRadiusFactor);
 
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
-    painter.setPen(QPen(circleBorderColor, 1.5));
-    painter.drawEllipse(center, circleRadius, circleRadius);
 
-    // Draw core's drop shadow
+    // Wave
+    waveColor.setAlphaF(1.0 - waveFactor);
+    painter.setPen(QPen(waveColor, 1.5));
+    painter.drawEllipse(center, waveFactor * radius - 0.5, waveFactor * radius - 0.5);
+
+    // Base
+    painter.setPen(Qt::NoPen);
+    painter.setBrush(baseColor);
+    painter.drawEllipse(center, 64, 64);
+
+    // Frame outer drop shadow
     painter.setBrush(Qt::NoBrush);
     painter.setPen(QPen(QColor("#10000000"), 1.5));
-    painter.drawEllipse(center + QPointF(0, 2), length / 5.0, length / 5.0);
+    painter.drawEllipse(center + QPointF(0, 1), 64, 64);
 
-    // Draw core's background
-    painter.setPen(Qt::NoPen);
-    painter.setBrush(QColor("#4BA086"));
-    painter.drawEllipse(center, length / 5.0, length / 5.0);
-
-    // Draw core's border
+    // Frame outer border
     painter.setBrush(Qt::NoBrush);
-    painter.setPen(QPen(QColor("#ffffff"), 1.5));
-    painter.drawEllipse(center, length / 5.0, length / 5.0);
-    painter.setPen(QPen(QColor("#f2f2f2"), 1.5));
-    painter.drawEllipse(center, length / 5.0 - 1, length / 5.0 - 1);
-    painter.setPen(QPen(QColor("#e8e8e8"), 1.5));
-    painter.drawEllipse(center, length / 5.0 - 2, length / 5.0 - 2);
-    painter.setPen(QPen(QColor("#e0e0e0"), 1.5));
-    painter.drawEllipse(center, length / 5.0 - 3, length / 5.0 - 3);
-    painter.setPen(QPen(QColor("#d0d0d0"), 1)); // #202528 for dark mode
-    painter.drawEllipse(center, length / 5.0 + 1, length / 5.0 + 1);
+    painter.setPen(QPen(frameBorderColor, 1));
+    painter.drawEllipse(center, 64, 64);
 
-    // Draw core's inner lines
-    painter.setPen("#40ffffff");
-    painter.drawEllipse(center, length / 7.0, length / 7.0);
-    painter.drawEllipse(center, length / 12.0, length / 12.0);
+    // Frame
+    painter.setBrush(Qt::NoBrush);
+    painter.setPen(QPen(frameColor, 1.5));
+    painter.drawEllipse(center, 64 - 1, 64 - 1);
+    painter.setPen(QPen(frameColor.darker(104), 1.5));
+    painter.drawEllipse(center, 64 - 2, 64 - 2);
+    painter.setPen(QPen(frameColor.darker(109), 1.5));
+    painter.drawEllipse(center, 64 - 3, 64 - 3);
+    painter.setPen(QPen(frameColor.darker(115), 1.5));
+    painter.drawEllipse(center, 64 - 4, 64 - 4);
 
-    // Draw core's inner lines
+    // Circles
+    painter.setBrush(Qt::NoBrush);
+    painter.setPen(circleColor);
+    painter.drawEllipse(center, 45, 45);
+    painter.drawEllipse(center, 25, 25);
+
+    // Bubbling circles
     static const qreal begins[] = {
         1.75 * M_PI,
         0.25 * M_PI,
@@ -85,48 +136,59 @@ void RadarWidget::paintEvent(QPaintEvent*)
         center + QPoint(35, 35),
         center + QPoint(-35, -5)
     };
+    painter.setBrush(Qt::NoBrush);
     for (size_t i = 0; i < sizeof(begins) / sizeof(begins[0]); ++i) {
         static const qreal DIFF = 1.25 * M_PI;
         if (needleFactor > begins[i] && needleFactor < begins[i] + DIFF) {
-            qreal factor = (needleFactor - begins[i]) / DIFF;
+            const qreal factor = (needleFactor - begins[i]) / DIFF;
             painter.setPen(QColor(255, 255, 255, 255 * (1 - factor)));
             painter.drawEllipse(points[i], factor * 6, factor * 6);
         }
     }
 
-    // Draw core's needle
-    painter.setPen(QPen(QColor("white"), 2.0));
-    painter.drawLine(center, center +
-                     QPointF(std::cos(m_needleAnimation.currentValue().toReal()) * (length / 5.0 - 5),
-                             std::sin(m_needleAnimation.currentValue().toReal()) * (length / 5.0 - 5)));
-
-    QConicalGradient grad2(center, -m_needleAnimation.currentValue().toReal() * 180 / M_PI);
-    grad2.setColorAt(0.0, "#50ffffff");
-    grad2.setColorAt(0.2, "#40ffffff");
-    grad2.setColorAt(0.5, "#10ffffff");
-    grad2.setColorAt(1.0, "#00ffffff");
-    painter.setBrush(grad2);
+    // Scanning effect
+    QConicalGradient scanningEffect(center, - needleFactor * 180 / M_PI);
+    scanningColor.setAlpha(140);
+    scanningEffect.setColorAt(0.0, scanningColor);
+    scanningColor.setAlpha(100);
+    scanningEffect.setColorAt(0.2, scanningColor);
+    scanningColor.setAlpha(30);
+    scanningEffect.setColorAt(0.5, scanningColor);
+    scanningColor.setAlpha(5);
+    scanningEffect.setColorAt(0.9, scanningColor);
+    painter.setBrush(scanningEffect);
     painter.setPen(Qt::NoPen);
-    painter.drawEllipse(center, length / 5.0 - 4, length / 5.0 - 4);
+    painter.drawEllipse(center, 64 - 5, 64 - 5);
 
-    // Draw core's border drop shadow
+    // Needle
     painter.setBrush(Qt::NoBrush);
-    painter.setPen(QPen(QColor("#25000000"), 1.5));
-    painter.drawEllipse(center, length / 5.0 - 4, length / 5.0 - 4);
-    painter.setPen(QPen(QColor("#05000000"), 1.5));
-    painter.drawEllipse(center, length / 5.0 - 5, length / 5.0 - 5);
+    painter.setPen(QPen(needleColor, 2.0));
+    painter.drawLine(center, center + QPointF(std::cos(needleFactor) * (64 - 6),
+                                              std::sin(needleFactor) * (64 - 6)));
 
-    // Draw core's needle ball
+    // Needle core drop shadow
+    painter.setBrush(Qt::NoBrush);
+    painter.setPen("#15000000");
+    painter.drawEllipse(center, 8.5, 8.5);
+
+    // Needle core
     QLinearGradient grad(0, 0, 0, 1);
     grad.setCoordinateMode(QGradient::ObjectMode);
-    grad.setColorAt(0, "#ffffff");
-    grad.setColorAt(1, "#e0e0e0");
+    grad.setColorAt(0, needleColor);
+    grad.setColorAt(1, needleColor.darker(115));
     painter.setBrush(grad);
     painter.setPen(Qt::NoPen);
     painter.drawEllipse(center, 8, 8);
 
-    // Draw core's needle ball drop shadow
+    // Frame inner drop shadow
     painter.setBrush(Qt::NoBrush);
-    painter.setPen("#15000000");
-    painter.drawEllipse(center, 8.5, 8.5);
+    painter.setPen(QPen(QColor("#20000000"), 1.5));
+    painter.drawEllipse(center, 64 - 5, 64 - 5);
+    painter.setPen(QPen(QColor("#05000000"), 1.5));
+    painter.drawEllipse(center, 64 - 6, 64 - 6);
+}
+
+QSize RadarWidget::sizeHint() const
+{
+    return {320, 320};
 }
--- radarwidget.h
+++ radarwidget.h
@@ -3,19 +3,40 @@
 
 #include <QWidget>
 #include <QVariantAnimation>
+#include <QBasicTimer>
 
 class RadarWidget : public QWidget
 {
     Q_OBJECT
+
+    enum {
+        REFRESH_RATE = 33 // 30 fps
+    };
+
 public:
     explicit RadarWidget(QWidget* parent = nullptr);
 
+    QAbstractAnimation::State state() const;
+
+    int period() const;
+
+public slots:
+    void start(int period = 2000);
+    void stop();
+    void pause();
+    void resume();
+
 protected:
     QSize sizeHint() const override;
+    void timerEvent(QTimerEvent* event) override;
     void paintEvent(QPaintEvent* event) override;
 
+signals:
+    void stateChanged(QAbstractAnimation::State newState, QAbstractAnimation::State oldState);
+
 private:
-    QVariantAnimation m_circleRadiusAnimation;
+    QBasicTimer m_updateTimer;
+    QVariantAnimation m_waveAnimation;
     QVariantAnimation m_needleAnimation;
 };
 
